da3255aab6fb97e0b18f298a0a41318e826fba07
Brlr2003 <o.alsafarti@ilibri.ai>
2023-09-03 16:06:59 +0700

    Added Scaffold Items and Models

---
+++ Quiz.Api/Models/Answer.cs
@@ -0,0 +1,11 @@
+﻿namespace Quiz.Api.Models
+{
+    public class Answer
+    {
+        public long Id { get; set; }
+        public long QuestionID { get; set; }
+        public required string AnswerText { get; set; }
+        public bool IsCorrect { get; set; }
+
+    }
+}
---
+++ Quiz.Api/Models/Question.cs
@@ -0,0 +1,9 @@
+﻿namespace Quiz.Api.Models
+{
+    public class Question
+    {
+        public long Id { get; set; }
+        public required string Description { get; set; }
+        public long SubjectID { get; set; }
+    }
+}
